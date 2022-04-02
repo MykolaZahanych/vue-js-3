01e84d551a3198bd5d813c3b416fe200e3409642
Mykola Zahanych <m.zahanych@gmail.com>
2022-04-02 20:05:12 +0200

    New post component

--- src/components/NewPost.vue
+++ src/components/NewPost.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>New Post</div>
+  <PostWriter :post="newPost" @save="save" />
 </template>
+
+<script lang="ts">
+import { Post } from '../mocks';
+import { defineComponent } from 'vue';
+import PostWriter from './PostWriter.vue';
+import moment from 'moment';
+
+export default defineComponent({
+  components: { PostWriter },
+  setup() {
+    const newPost: Post = {
+      id: '-1',
+      title: 'New post',
+      created: moment(),
+    };
+
+    const save = (post: Post) => {
+      console.log('save emit', post);
+    };
+
+    return { newPost, save };
+  },
+});
+</script>
---
+++ src/components/PostWriter.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">New Post</div>
+        <input v-model="title" type="text" class="input" />
+      </div>
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <div contenteditable ref="contentEditable" @input="handleInput" />
+    </div>
+    <div class="column">
+      <div v-html="html" />
+    </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <button @click="save" class="button is-primary is-pulled-right">
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Post } from '../mocks';
+import { defineComponent, onMounted, ref, watch, watchEffect } from 'vue';
+import { parse } from 'marked';
+import highlight from 'highlight.js';
+import debounce from 'lodash/debounce';
+
+export default defineComponent({
+  props: {
+    post: {
+      type: Object as () => Post,
+      required: true,
+    },
+  },
+
+  emits: {
+    save: (post: Post) => {
+      return true;
+    },
+  },
+
+  setup(props, ctx) {
+    const title = ref(props.post.title);
+    const content = ref('## Title\nEnter your post content');
+    const html = ref('');
+    const contentEditable = ref<HTMLDivElement | null>(null);
+
+    const parseHtml = (str: string) => {
+      html.value = parse(str, {
+        gfm: true,
+        breaks: true,
+        highlight: (code: string) => {
+          return highlight.highlightAuto(code).value;
+        },
+      });
+    };
+
+    watch(
+      content,
+      debounce((newVal) => {
+        parseHtml(newVal);
+      }, 250),
+      { immediate: true }
+    );
+
+    // watchEffect(() => {
+    //   html.value = parse(content.value, {
+    //     gfm: true,
+    //     breaks: true,
+    //     highlight: (code: string) => {
+    //       return highlight.highlightAuto(code).value;
+    //     },
+    //   });
+    // });
+
+    const handleInput = () => {
+      content.value = contentEditable.value?.innerText || '';
+    };
+
+    onMounted(() => {
+      if (!contentEditable.value) {
+        throw new Error('This should never happen');
+      }
+      contentEditable.value.innerText = content.value;
+    });
+
+    const save = () => {
+      const newPost: Post = {
+        ...props.post,
+        title: title.value,
+        html: html.value,
+        markdown: content.value,
+      };
+
+      ctx.emit('save', newPost);
+    };
+
+    return {
+      save,
+      html,
+      title,
+      content,
+      contentEditable,
+      handleInput,
+    };
+  },
+});
+</script>
+
+<style>
+.column {
+  overflow-y: scroll;
+}
+</style>
