f618a82c98c032d68d83af757c41a1523a5aaede
Mykola Zahanych <m.zahanych@gmail.com>
2022-04-03 11:36:18 +0200

    teleport modal

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+  <div class="modal" :style="style">
+    <div class="modal-background"></div>
+    <div class="modal-content">
+      <div id="modal" />
+    </div>
+    <button @click="hide" class="modal-close is-large"></button>
+  </div>
+
   <section class="section">
     <div class="container">
       <NavBar />
@@ -8,12 +16,29 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, StyleValue } from 'vue';
 import NavBar from './components/NavBar.vue';
+import { useModal } from './useModal';
 
 export default defineComponent({
   name: 'App',
   components: { NavBar },
+  setup() {
+    const modal = useModal();
+
+    const style = computed(() => {
+      return {
+        display: modal.show.value ? 'block' : 'none',
+      };
+    });
+
+    return {
+      style,
+      hide: () => {
+        modal.hideModal();
+      },
+    };
+  },
 });
 </script>
 
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,8 +2,29 @@
   <div class="navbar">
     <div class="navbar-end">
       <div class="buttons">
+        <button class="button" @click="show">Sign Up</button>
         <router-link to="/posts/new" class="button">New Post</router-link>
       </div>
     </div>
   </div>
+  <teleport to="#modal">
+    <div>Modal</div>
+  </teleport>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useModal } from '../useModal';
+
+export default defineComponent({
+  setup() {
+    const modal = useModal();
+
+    return {
+      show: () => {
+        modal.showModal();
+      },
+    };
+  },
+});
+</script>
